7e7af6e5dd253f05a96f0df4bb6012652d233798
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-10-16 10:10:25 +0530

    Time: 52 ms (55.56%), Space: 19.7 MB (55.56%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -4,12 +4,12 @@ public:
         sort(nums.begin() , nums.end());
         int i = 0 , j = nums.size()-1;
         int k = 0 ;
-        while(k<nums.size()){
+        while(i<=j){
             if(-nums[i]==nums[j]) return nums[j];
             
             else if(-nums[i]<nums[j]) j--;
             else i++;
-            k++;
+            
         }
         return -1;
         
