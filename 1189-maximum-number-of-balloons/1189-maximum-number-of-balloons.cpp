e7a35c27a1610c8a470a7e6a400236069f267d83
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-10-16 11:13:26 +0530

    Time: 7 ms (50.52%), Space: 6.7 MB (7.07%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string s) {
+        unordered_map<char , int> mp;
+        for(int i = 0 ; i<s.size() ; i++)
+            mp[s[i]]++;
+        
+        return min({mp['b'] , mp['a'] , mp['l']/2 , mp['o']/2 , mp['n']});
+        
+    }
+};
