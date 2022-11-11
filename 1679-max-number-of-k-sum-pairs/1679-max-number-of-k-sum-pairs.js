eea3a9921d772fef92c1d8eee58853d0e18e111a
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-11-11 18:10:59 +0530

    Time: 4832 ms (5.26%), Space: 52.5 MB (94.74%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -1,18 +1,19 @@
-class Solution {
-public:
-    int maxOperations(vector<int>& nums, int k) {
-        int count = 0;
-        unordered_map<int , int> mp;
-        for(int i = 0 ; i<nums.size() ; i++){
-            int a =  k - nums[i];
-            if(mp[a]>0){
-                count++;
-                mp[a]--;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function(nums, k) {
+    let n = 0;
+    for(let i = 0 ; i<nums.length ; i++){
+        for(let j = i+1 ; j<nums.length ; j++){
+            if(nums[i]+nums[j]===k){
+                nums.splice(j,1);
+                n++;
+                break;
+                
             }
-            else
-                mp[nums[i]]++;
         }
-        return count;
-        
     }
+    return n;
 };
